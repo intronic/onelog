587ee0c823d9ee67dc20a16052f14c5669918e6e
Michael Pheasant <mpheasant@gmail.com>
2014-12-01 21:58:42 +1000

    toggle version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onelog "0.4.5"
+(defproject intronic/onelog "0.4.6"
   :description "Batteries-included logging for Clojure"
   :url "https://github.com.com/pjlegato/onelog"
   :license {:name "Eclipse Public License"
